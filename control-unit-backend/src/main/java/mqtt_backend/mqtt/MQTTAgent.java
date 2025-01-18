b262b1cdb17038a9e92d92872f05d99b53cf360d
elzemaltun <elzemaltunn@gmail.com>
2025-01-18 01:29:47 +0100

    created initial step of the MQTT communication.

---
+++ control-unit-backend/src/main/java/mqtt_backend/mqtt/MQTTAgent.java
@@ -0,0 +1,53 @@
+package mqtt_backend.mqtt;
+
+import io.netty.handler.codec.mqtt.MqttQoS;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.mqtt.MqttClient;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MQTTAgent extends AbstractVerticle{
+	
+	private static final String BROKER_ADRESS = "broker.mqtt-dashboard.com";
+    private static final String TOPIC_NAME = "temperature";
+
+    // Store the received temperature messages
+    private List<String> messageHistory = new ArrayList<>();
+
+    // Constructor for initalization
+    public MQTTAgent() {
+	}
+
+    // Override start method in the AbstractVerticle class
+    @Override
+    public void start() {		
+		MqttClient client = MqttClient.create(vertx);
+        client.connect(1883, BROKER_ADRESS, c-> {
+
+			client.publishHandler(s -> {
+			  System.out.println(s.topicName() + ": " + s.payload().toString());
+			  System.out.println("QoS: " + s.qosLevel());
+
+              String temperature = s.payload().toString();
+              messageHistory.add(temperature);
+			})
+			.subscribe(TOPIC_NAME, 2);		
+
+			client.publish(TOPIC_NAME,
+				  Buffer.buffer("Sent by MQTT Agent"),
+				  MqttQoS.AT_LEAST_ONCE,
+				  false,
+				  false);
+		}); 
+    }
+
+}
---
+++ control-unit-backend/src/main/java/mqtt_backend/mqtt/Main.java
@@ -0,0 +1,11 @@
+package mqtt_backend.mqtt;
+import io.vertx.core.Vertx;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {		
+		Vertx vertx = Vertx.vertx();
+		MQTTAgent agent = new MQTTAgent();
+		vertx.deployVerticle(agent);
+	}
+}
